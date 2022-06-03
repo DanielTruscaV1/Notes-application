2390f9e7f589a1bb93f1cfbf92fcf7c9f34fef18
DanielTruscaV1 <danieltrusca2016@gmail.com>
2022-06-03 18:11:04 +0300

    Added a new feature in which the user can choose what color the note will have.

--- src/components/Note.js
+++ src/components/Note.js
@@ -4,7 +4,12 @@ import {MdDeleteForever} from "react-icons/md";
 const Note = ({id, text, date, handleDeleteNote}) => {
     return (
         <div className="note">
-            <span>{text}</span>
+            <div className="inline">
+                {text}
+                <select>
+                    <option>Color</option>
+                </select>
+            </div>
             <div className="note-footer">
                 <small>{date}</small>
                 <MdDeleteForever onClick={() => handleDeleteNote(id)} className="delete-icon" size="1.3em"/>
